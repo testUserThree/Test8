23e9e3306bf4a695a7088b3baae9532452a04bac
WeiYaoJia <weiyaojia@weiyaojiademacbook-pro.local>
2018-12-17 20:48:53 +0800

    Initial Commit

---
+++ Test8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test8
+//
+//  Created by WeiYaoJia on 2018/12/17.
+//  Copyright © 2018年 SiCNU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
